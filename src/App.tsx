ed2d02b186e8bba30f5c32516dca470713a3ee6c
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-02-11 09:46:49 +0200

    create TodosList component and integrate it into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Todo } from './types/Todo';
 import { getTodos } from './api/todos';
 import classNames from 'classnames';
 import { Header } from './components/Header';
+import { TodosList } from './components/TodosList/TodosList';
 import { Footer } from './components/Footer/Footer';
 
 export const App: React.FC = () => {
@@ -60,106 +61,7 @@ export const App: React.FC = () => {
 
       <div className="todoapp__content">
         <Header />
-        {todos.length > 0 && (
-          <section className="todoapp__main" data-cy="TodoList">
-            {filteredTodos.map(todo => (
-              <div
-                data-cy="Todo"
-                className={classNames('todo', { completed: todo.completed })}
-                key={todo.id}
-              >
-                <label className="todo__status-label">
-                  <input
-                    data-cy="TodoStatus"
-                    type="checkbox"
-                    className="todo__status"
-                    checked={todo.completed}
-                    onChange={() => {}}
-                  />
-                </label>
-
-                <span data-cy="TodoTitle" className="todo__title">
-                  {todo.title}
-                </span>
-
-                {/* Remove button appears only on hover */}
-                <button
-                  type="button"
-                  className="todo__remove"
-                  data-cy="TodoDelete"
-                >
-                  ×
-                </button>
-
-                {/* overlay will cover the todo while it is being deleted or updated */}
-                <div data-cy="TodoLoader" className="modal overlay">
-                  <div className="modal-background has-background-white-ter" />
-                  <div className="loader" />
-                </div>
-              </div>
-            ))}
-            {/* This is a completed todo */}
-
-            {/* This todo is an active todo */}
-
-            {/* This todo is being edited */}
-
-            {/* This todo is in loadind state */}
-          </section>
-        )}
-        {/* Hide the footer if there are no todos */}
-        {todos.length > 0 && (
-          <footer className="todoapp__footer" data-cy="Footer">
-            <span className="todo-count" data-cy="TodosCounter">
-              {activeTodosCounter} items left
-            </span>
-
-            {/* Active link should have the 'selected' class */}
-            <nav className="filter" data-cy="Filter">
-              <a
-                href="#/"
-                className={classNames('filter__link', {
-                  selected: filterBy === 'all',
-                })}
-                data-cy="FilterLinkAll"
-                onClick={() => setFilterBy('all')}
-              >
-                All
-              </a>
-
-              <a
-                href="#/active"
-                className={classNames('filter__link', {
-                  selected: filterBy === 'active',
-                })}
-                data-cy="FilterLinkActive"
-                onClick={() => setFilterBy('active')}
-              >
-                Active
-              </a>
-
-              <a
-                href="#/completed"
-                className={classNames('filter__link', {
-                  selected: filterBy === 'completed',
-                })}
-                data-cy="FilterLinkCompleted"
-                onClick={() => setFilterBy('completed')}
-              >
-                Completed
-              </a>
-            </nav>
-
-            {/* this button should be disabled if there are no completed todos */}
-            <button
-              type="button"
-              className="todoapp__clear-completed"
-              data-cy="ClearCompletedButton"
-            >
-              Clear completed
-            </button>
-          </footer>
-        )}
+        <TodosList todos={todos} filteredTodos={filteredTodos} />
         <Footer
           todos={todos}
           activeTodosCounter={activeTodosCounter}
@@ -185,14 +87,6 @@ export const App: React.FC = () => {
         />
         {/* show only one message at a time */}
         {errorMessage}
-        {/*<br />*/}
-        {/*Title should not be empty*/}
-        {/*<br />*/}
-        {/*Unable to add a todo*/}
-        {/*<br />*/}
-        {/*Unable to delete a todo*/}
-        {/*<br />*/}
-        {/*Unable to update a todo*/}
       </div>
     </div>
   );
